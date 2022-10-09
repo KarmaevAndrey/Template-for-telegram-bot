95b41a2c367275f699f8ce704b5effa5b96e69cd
Ortyom <115371736+Ortyom-bot@users.noreply.github.com>
2022-10-09 21:16:18 +0300

    Update utis.py

--- utis.py
+++ utis.py
@@ -3,10 +3,9 @@ from telebot import types
 from baza_working import *
 from setings import *
 
-
 admin_id = 912185600
 my_bot = "Change_the_name_of_the_bot"
 path_to_baza = "media/baza.json"
 text_button_text = "¿Что-то?"
 text_error = "😭ОЙ.., Что-то пошло не так попробуйте ещё раз😕"
-tinkoff_url = "https://www.tinkoff.ru/cf/9zGNbntzKcQ"
+tinkoff_url = "https://www.tinkoff.ru/rm/bitsa.artem1/qgLo799929"
